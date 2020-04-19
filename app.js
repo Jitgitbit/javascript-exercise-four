9b6ce8d95f37871a4f98e508f8979f424f9615d4
Jitgitbit <rowowolf@gmail.com>
2020-04-19 13:20:39 +0200

    basic vars ready

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {})
+document.addEventListener('DOMContentLoaded', () => {
+  const squares = document.querySelectorAll('.grid div')
+  const scoreDisplay = document.querySelector('span')
+  const startBtn = document.querySelector('.start')
+})
